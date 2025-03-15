3834fab84f5d87813368f7ba80bf28643b26f845
Cris0291 <cris0291@gmail.com>
2025-03-14 21:40:56 -0600

    Add more changes to the tokenizer

--- Tokenizer/Tokenizer.cpp
+++ Tokenizer/Tokenizer.cpp
@@ -1,9 +1,47 @@
-module Tokenizer;
-using namespace std;
+#include "Tokenizer.h"
 
-template<typename InputIterator>
-Tokenizer<InputIterator>::Tokenizer(std::string&& json) noexcept{
-    json_value = std::move(json);
-    current_position = std::begin()
-};
+template<typename IteratorType>
+Tokenizer<IteratorType>::Tokenizer(std::string &&json) noexcept : json_value{std::move(json)}, current_position{std::ranges::cbegin(json)}, position{0} {
+}
+
+template<typename IteratorType>
+void Tokenizer<IteratorType>::parser() {
+    if (json_value[0] != '{') throw std::invalid_argument{"Json value should start with { and end with }"};
+    char next_token = get_next_token_without_spaces();
+
+    switch (next_token) {
+        case next_token == '{':
+            break;
+        case next_token == '}':
+            break;
+        case next_token == '[':
+            break;
+        case next_token == ']':
+            break;
+        case next_token == '"':
+            break;
+        case next_token == '-' || (next_token >= '0' && next_token <= '9'):
+            break;
+        case next_token == 't':
+            break;
+        case next_token == 'f':
+            break;
+    }
+}
+
+template<typename IteratorType>
+char Tokenizer<IteratorType>::get_next_token_without_spaces() {
+    char result {};
+    while (true) {
+        if (*current_position == " " || *current_position == "\n") {
+            current_position++;
+            continue;
+        }
+        result = *current_position;
+        break;
+    }
+
+    return  result;
+}
+
 
