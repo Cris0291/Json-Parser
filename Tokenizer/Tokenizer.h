4cb10cffbeac1f17b914d4c0b77c82de0d044ae9
Cris0291 <cris0291@gmail.com>
2025-03-14 21:44:02 -0600

    Add header file instead of module due to problems

--- Tokenizer/Tokenizer.h
+++ Tokenizer/Tokenizer.h
@@ -1,14 +1,20 @@
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#endif
+
 #include <string>
 #include <iterator>
 
-export module Tokenizer;
-
-export template<typename IteratorType>
+template<typename IteratorType>
 class Tokenizer{
-  public:
+public:
     Tokenizer(std::string&& json) noexcept;
     void parser();
-  private:
+private:
     std::string json_value;
     typename std::iterator_traits<IteratorType>::value_type current_position;
+    int position;
+    char get_next_token_without_spaces();
 };
