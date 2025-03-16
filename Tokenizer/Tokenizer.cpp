9f6b415d3d871098e7aaa82fe0e8a55d79fb7ff3
Cris0291 <cris0291@gmail.com>
2025-03-15 21:42:10 -0600

    I will change the parse function so that it works by changing the state

--- Tokenizer/Tokenizer.cpp
+++ Tokenizer/Tokenizer.cpp
@@ -1,14 +1,40 @@
 #include "Tokenizer.h"
 
+#include <ranges>
+
 template<typename IteratorType>
 Tokenizer<IteratorType>::Tokenizer(std::string &&json) noexcept : json_value{std::move(json)}, current_position{std::ranges::cbegin(json)}, position{0} {
 }
 
-template<typename IteratorType>
-void Tokenizer<IteratorType>::parser() {
-    if (json_value[0] != '{') throw std::invalid_argument{"Json value should start with { and end with }"};
-    char next_token = get_next_token_without_spaces();
 
+
+template<typename IteratorType>
+void Tokenizer<IteratorType>::parse() {
+    if (json_value[0] != '{') throw std::invalid_argument{"Json value should start with { and end with }"};
+
+
+
+}
+
+template<typename IteratorType>
+char Tokenizer<IteratorType>::get_next_token_without_spaces() {
+    char result {};
+    while (true) {
+        if (*current_position == " " || *current_position == "\n") {
+            current_position++;
+            continue;
+        }
+        result = *current_position;
+        break;
+    }
+
+    return  result;
+}
+
+
+template<typename IteratorType>
+void Tokenizer<IteratorType>::check_token() {
+    char next_token = get_next_token_without_spaces();
     switch (next_token) {
         case next_token == '{':
             break;
@@ -28,20 +54,3 @@ void Tokenizer<IteratorType>::parser() {
             break;
     }
 }
-
-template<typename IteratorType>
-char Tokenizer<IteratorType>::get_next_token_without_spaces() {
-    char result {};
-    while (true) {
-        if (*current_position == " " || *current_position == "\n") {
-            current_position++;
-            continue;
-        }
-        result = *current_position;
-        break;
-    }
-
-    return  result;
-}
-
-
--- Tokenizer/Tokenizer.h
+++ Tokenizer/Tokenizer.h
@@ -11,10 +11,11 @@ template<typename IteratorType>
 class Tokenizer{
 public:
     Tokenizer(std::string&& json) noexcept;
-    void parser();
+    void parse();
 private:
     std::string json_value;
     typename std::iterator_traits<IteratorType>::value_type current_position;
     int position;
     char get_next_token_without_spaces();
+    void check_token();
 };
