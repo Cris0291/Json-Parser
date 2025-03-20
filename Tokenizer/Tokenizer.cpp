35169b601e57e58ac4b34d723c821b02dd6a1b87
Cris0291 <cris0291@gmail.com>
2025-03-19 20:06:22 -0600

    Keep working on the tokenizer

--- Tokenizer/Tokenizer.cpp
+++ Tokenizer/Tokenizer.cpp
@@ -3,6 +3,7 @@
 
 #include <ranges>
 
+#include "LookUpTable.h"
 #include "TokenNode.h"
 using namespace State;
 using namespace  Node;
@@ -21,12 +22,31 @@ void Tokenizer::Parse() {
 
     TokenNode tokenNode{};
 
+    int curlybraceBalance{0};
+
     while (currChar != json_value.end()) {
         switch (stateNow) {
             case TokenState::NewToken: {
                 tokenNode = TokenNode{Type::Unknown, Value{0}};
+
+                if (lut::WhitespaceDigits.at((currChar[0]))) {
+                    ++currChar;
+                    stateNext = TokenState::NewToken;
+                }
+                else if (currChar[0] == '"') {
+                    stateNext = TokenState::Key;
+                }
+                else if (currChar[0] == '{') {
+                    ++curlybraceBalance;
+                    stateNext = TokenState::Open_Parenthesis;
+                }
+                else if (currChar[0] == '}') {
+                    --curlybraceBalance;
+                    stateNext = TokenState::Close_Parenthesis;
+                }
             }
         }
+        stateNow = stateNext;
     }
 }
 
