c2f0a913062e68f933d630b3efa3752ba3d15501
Cris0291 <cris0291@gmail.com>
2025-03-19 20:07:09 -0600

    Add the rest of the lookups

--- Tokenizer/LookUpTable.h
+++ Tokenizer/LookUpTable.h
@@ -3,6 +3,7 @@
 #define LOOKUPTABLE_H
 #include <array>
 #include <string_view>
+#include <cstdint>
 
 #endif //LOOKUPTABLE_H
 
@@ -10,7 +11,15 @@ namespace lut {
     static constexpr auto MakeTable(const std::string_view acceptedCharacters) {
         std::array<bool, 256> lut{0};
         for (const auto s : acceptedCharacters) {
-            lut.at(uint8_t(s));
+            lut.at(static_cast<uint8_t>(s)) = true;
         }
+
+        return lut;
     }
+
+    constexpr auto WhitespaceDigits = MakeTable(" \t\n\r\v\f");
+    constexpr auto NumericDigits = MakeTable("0123456789");
+    constexpr auto RealNumericDigits = MakeTable(".0123456789");
+    constexpr auto KeyDigits = MakeTable("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789");
+    constexpr auto StringDigits = MakeTable("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 }
