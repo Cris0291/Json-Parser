dfd1c94b65764112b4b4102e12fd2e570f4a67db
Cris0291 <cris0291@gmail.com>
2025-03-22 20:06:27 -0600

    Keep working on those tokens

--- Tokenizer/Tokenizer.cpp
+++ Tokenizer/Tokenizer.cpp
@@ -16,26 +16,37 @@ Tokenizer::Tokenizer(std::string &&json) noexcept : json_value{std::move(json)} 
 
 void Tokenizer::Parse() {
     auto currChar = json_value.begin();
+    std::vector<Token> vectorOutputTokens {};
+    bool initMarker {true};
+    bool keyStartMarker {true};
 
     TokenState stateNow = TokenState::NewToken;
     TokenState stateNext = TokenState::NewToken;
+    JsonState jsonStateNow = JsonState::Unknown;
 
-    TokenNode tokenNode{};
+    std::string tokenValue {""};
+    Token token{};
 
     int curlybraceBalance{0};
-    std::string tokenValue {""};
+
 
     while (currChar != json_value.end()) {
         switch (stateNow) {
             case TokenState::NewToken: {
-                tokenNode = TokenNode{Type::Unknown, Value{0}};
+                tokenValue.clear();
+                token = {TokenState::Unknown, JsonState::Unknown, ""};
 
                 if (lut::WhitespaceDigits.at((currChar[0]))) {
                     ++currChar;
                     stateNext = TokenState::NewToken;
                 }
                 else if (currChar[0] == '"') {
-                    stateNext = TokenState::StringLiteral;
+                    if (jsonStateNow == JsonState::Init) {
+                        stateNext = TokenState::Key;
+                    }
+                    else {
+                        stateNext = TokenState::StringLiteral;
+                    }
                 }
                 else if (currChar[0] == '{') {
                     ++curlybraceBalance;
@@ -66,6 +77,21 @@ void Tokenizer::Parse() {
                 else if (currChar[0] == 'f') {
                     stateNext = TokenState::Boolean;
                 }
+                break;
+            }
+            case TokenState::Key: {
+                if (keyStartMarker) {
+                    if (!lut::KeyValidStart.at(currChar[0]))throw std::invalid_argument("Json key should start with letters or an underscore");
+                    keyStartMarker = false;
+                }
+                if (currChar[0] == '"') {
+                    token = {TokenState::Key, JsonState::Key, tokenValue};
+                    jsonStateNow = JsonState::Key;
+                    stateNext = TokenState::CompleteToken;
+                }
+                tokenValue += currChar[0];
+                ++currChar;
+                break;
             }
             case TokenState::StringLiteral: {
                 if (currChar[0] != '"') {
@@ -79,9 +105,31 @@ void Tokenizer::Parse() {
             }
             case TokenState::Open_Array: {
                 tokenValue += currChar[0];
+                token = {TokenState::Open_Array, tokenValue};
                 ++currChar;
                 stateNext = TokenState::NewToken;
             }
+            case TokenState::Open_Parenthesis: {
+                tokenValue += currChar[0];
+                stateNext = TokenState::CompleteToken;
+
+                if (initMarker && std::distance(json_value.begin(), currChar) == 0) {
+                    token = {TokenState::Open_Parenthesis, JsonState::Init, tokenValue};
+                    jsonStateNow = JsonState::Init;
+                    initMarker = false;
+                }
+                else {
+                    token = {TokenState::Open_Parenthesis, JsonState::Value, tokenValue};
+                    jsonStateNow = JsonState::Value;
+                }
+                break;
+            }
+            case TokenState::CompleteToken: {
+                vectorOutputTokens.push_back(token);
+                stateNext = TokenState::NewToken;
+                break;
+            }
+
         }
         stateNow = stateNext;
     }
